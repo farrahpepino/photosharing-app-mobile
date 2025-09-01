831d5476af5237f053eff73b885de3970e3f27a9
farrahpepino <idcfarrah@gmail.com>
2025-09-01 01:51:18 -0400

    clean up ?

--- client/App.tsx
+++ client/App.tsx
@@ -3,5 +3,13 @@ import React from 'react';
 import StackNav from './src/navigation/StackNav';
 
 export default function App() {
+  const originalLog = console.log;
+  console.log = (...args) => {
+    if (typeof args[0] === "string" && args[0].includes("heartbeats")) {
+      return;
+    }
+    originalLog(...args);
+  };
+
   return <StackNav />;
 }
